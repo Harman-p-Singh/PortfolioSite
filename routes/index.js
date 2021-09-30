cf11c34565aa77113e975c6ca4da8c0b243a7649
Harman Singh <hsin1318@my.centennialcollege.ca>
2021-09-29 20:47:35 -0400

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('index', { title: 'Home', page: 'home'});
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('index', { title: 'Home', page:'home'});
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('index', { title: 'About', page:'about'});
 });
 
 /* GET Products page. */
 router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+  res.render('index', { title: 'Products', page:'products'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+  res.render('index', { title: 'Services', page:'services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+  res.render('index', { title: 'Contact', page:'contact'});
 });
 
 module.exports = router;
